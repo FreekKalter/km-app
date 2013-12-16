8b7d84d78a5adac089f5445a22594ac805475eb3
Freek Kalter <freek@kalteronline.org>
2013-12-16 23:09:33 +0100

    cache favicon wicht cachehandler function

--- app/km.go
+++ app/km.go
@@ -332,31 +332,33 @@ func init() {
 	dbmap.TraceOn("[gorp]", log.New(logFile, "myapp:", log.Lmicroseconds))
 }
 
-func cacheHandler(days int, h http.Handler) http.Handler {
+func cacheHandler(h http.Handler, days int) http.Handler {
 	dur := time.Duration(days) * time.Duration(24) * time.Hour
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// ourHandler implements the http.Handler interface
+	ourHandler := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Cache-Control", fmt.Sprintf("max-age=%d, public, must-revalidate, proxy-revalidate", int64(dur.Seconds())))
 		h.ServeHTTP(w, r)
-	})
+	}
+	return http.HandlerFunc(ourHandler)
 }
 
 func main() {
 	defer dbmap.Db.Close()
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler)
-	r.HandleFunc("/state/{id}", stateHandler)
+
+	// static files get served directly
+	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/", cacheHandler(http.FileServer(http.Dir("js/")), 30)))
+	r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", cacheHandler(http.FileServer(http.Dir("img/")), 30)))
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", cacheHandler(http.FileServer(http.Dir("css/")), 30)))
+	r.PathPrefix("/partials/").Handler(http.StripPrefix("/partials/", cacheHandler(http.FileServer(http.Dir("partials/")), 30)))
+
+	r.Handle("/favicon.ico", cacheHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "favicon.ico") }), 40))
+
+	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/state/{id}", stateHandler).Methods("GET")
 	r.HandleFunc("/save", saveHandler).Methods("POST")
 	r.HandleFunc("/overview/{category}/{year}/{month}", overviewHandler).Methods("GET")
 	r.HandleFunc("/delete/{id}", deleteHandler).Methods("GET")
-
-	// static files get served directly
-	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/", cacheHandler(30, http.FileServer(http.Dir("js/")))))
-	r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", cacheHandler(30, http.FileServer(http.Dir("img/")))))
-	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", cacheHandler(30, http.FileServer(http.Dir("css/")))))
-	r.PathPrefix("/partials/").Handler(http.StripPrefix("/partials/", cacheHandler(30, http.FileServer(http.Dir("partials/")))))
-	r.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "favicon.ico")
-	})
 
 	http.Handle("/", r)
 	slog.Println("started...")
