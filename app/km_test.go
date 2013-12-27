814a87870d6c8e4539d376a4af15aa3ee70e873f
Freek Kalter <freek@kalteronline.org>
2013-12-27 22:02:44 +0100

    basic tabledriven test for all handlers

--- app/km_test.go
+++ app/km_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -13,11 +16,6 @@ import (
 var config Config = Config{Env: "production", Log: ""}
 var s *Server = NewServer("km_test", config)
 var db *gorp.DbMap = s.Dbmap
-
-type response struct {
-	body string
-	code int
-}
 
 func clearTable(t *testing.T, tableName string) {
 	_, err := db.Exec("truncate kilometers")
@@ -39,21 +37,21 @@ func tableDrivenTest(t *testing.T, table []*TestCombo) {
 		s.ServeHTTP(w, tc.req)
 		resp := tc.resp
 
-		if b := w.Body.String(); !strings.Contains(b, resp.body) {
-			t.Fatalf("body = %q, want '%s'", b, resp.body)
+		if b := w.Body.String(); !strings.Contains(b, resp.Body) {
+			t.Fatalf("body = %q, want '%s'", b, resp.Body)
 		}
-		if w.Code != resp.code {
-			t.Fatalf("code = %d, want %d", w.Code, resp.code)
+		if w.Code != resp.Code {
+			t.Fatalf("code = %d, want %d", w.Code, resp.Code)
 		}
 	}
 }
 
 type TestCombo struct {
 	req  *http.Request
-	resp response
+	resp Response
 }
 
-func NewTestCombo(url string, resp response) *TestCombo {
+func NewTestCombo(url string, resp Response) *TestCombo {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
@@ -64,10 +62,20 @@ func NewTestCombo(url string, resp response) *TestCombo {
 func TestDelete(t *testing.T) {
 	clearTable(t, "kilometers")
 
+	// add a row, save id
+	k := Kilometers{Begin: 1234}
+	err := db.Insert(&k)
+	if err != nil {
+		t.Fatal("TestDelete: dberror on insert", err)
+	}
+	id := strconv.FormatInt(k.Id, 10)
+
 	var table []*TestCombo = []*TestCombo{
-		NewTestCombo("/delete/1", response{"Unknown id.", 500}),
-		NewTestCombo("/delete/a", response{"Invalid id.", 500}),
-		NewTestCombo("/delete/-1", response{"Invalid id.", 500}),
+		NewTestCombo("/delete/1", InvalidId),
+		NewTestCombo("/delete/a", InvalidId),
+		NewTestCombo("/delete/-1", InvalidId),
+		// delete saved row, compare returned id
+		NewTestCombo("/delete/"+id, Response{id + "\n", 200}),
 	}
 	tableDrivenTest(t, table)
 }
@@ -76,10 +84,58 @@ func TestSave(t *testing.T) {
 	clearTable(t, "kilometers")
 
 	var table []*TestCombo = []*TestCombo{
-		NewTestCombo("/save/a", response{"404 page not found\n", 404}),
+		NewTestCombo("/save", NotFound),
+		NewTestCombo("/save/a", NotFound),
 	}
 	req, _ := http.NewRequest("POST", "/save", strings.NewReader(`{"Name": "Begin", "Value": 1234}`))
-	table = append(table, &TestCombo{req, response{"ok", 200}})
+	table = append(table, &TestCombo{req, Ok})
+
+	req, _ = http.NewRequest("POST", "/save", strings.NewReader(`{"Name": "Begin", "Value": "abc"}`))
+	table = append(table, &TestCombo{req, NotParsable})
+
+	req, _ = http.NewRequest("POST", "/save", strings.NewReader(`{"Name": "InvalidFieldname", "Value": 1234}`))
+	table = append(table, &TestCombo{req, UnknownField})
 
 	tableDrivenTest(t, table)
+}
+
+func TestHome(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	s.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Fatal("expected 200 got ", w.Code)
+	}
+}
+
+func TestState(t *testing.T) {
+	var table []*TestCombo = []*TestCombo{
+		NewTestCombo("/state", NotFound),
+		NewTestCombo("/state/2234a", InvalidId),
+	}
+	tableDrivenTest(t, table)
+
+	req, _ := http.NewRequest("GET", "/state/today", nil)
+	w := httptest.NewRecorder()
+	s.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Fatal("expected 200 got ", w.Code)
+	}
+	var unMarschalled interface{}
+	err := json.Unmarshal(w.Body.Bytes(), &unMarschalled)
+	if err != nil {
+		t.Fatal("/state/today not a valid json response:", err)
+	}
+}
+
+func TestOverview(t *testing.T) {
+	var table []*TestCombo = []*TestCombo{
+		NewTestCombo("/overview", NotFound),
+		NewTestCombo("/overview/invalidCategory/2013/01", InvalidUrl),
+		NewTestCombo("/overview/tijden/201a/01", InvalidUrl),
+		NewTestCombo("/overview/tijden/2013/0a", InvalidUrl),
+		NewTestCombo("/overview/1/2013/01", InvalidUrl),
+	}
+	tableDrivenTest(t, table)
+
 }
