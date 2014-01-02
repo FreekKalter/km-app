92a8dfe537507fc568bbcf3dd2f869769d3cae41
Freek Kalter <freek@kalteronline.org>
2014-01-02 20:55:31 +0100

    preparing db backup system

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ FROM freekkalter/postgres-supervisord:km
 ADD app /app
 ADD supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
+EXPOSE 5432
+
 CMD ["/usr/bin/supervisord"]
--- fabfile.py
+++ fabfile.py
@@ -17,29 +17,24 @@ def prepareDeploy():
 
 def remoteDeploy():
     buildNumber = os.environ['BUILD_NUMBER']
-    cidfile = '/home/fkalter/.km.cidfile'
     run("docker pull freekkalter/km")
-    run("docker kill `cat {}`".format(cidfile))
-    run("rm {}".format(cidfile))
-    run("docker run -cidfile={}\
-                     -v /home/fkalter/km/postgresdata:/data:rw\
-                     -v /home/fkalter/km/log:/log\
-                     -d -p 4001:4001 -p 5432:5432\
-                     freekkalter/km:{} /usr/bin/supervisord".format(cidfile, buildNumber))
+    runBuildNr(buildNumber)
 
-# Rollback excepts a excact buildnumber or a negative offset to rollback that number builds
 def rollback():
     # find latest buildnumber on remote, default = the build before the last one
     bn = int(run("docker images | awk '{ if(match($2, /^[0-9]+$/)) print $2}' | sort | tail -n1"))
     buildNumber = int(prompt('Rever to buildnumber: ', validate=int, default=bn-1))
     if buildNumber < 0:
         buildNumber = bn+buildNumber
+    runBuildNr(buildNumber)
 
+def runBuildNr(buildNumber):
     cidfile = '/home/fkalter/.km.cidfile'
     run("docker kill `cat {}`".format(cidfile))
     run("rm {}".format(cidfile))
-    run("docker run -cidfile={}\
-                     -v /home/fkalter/km/postgresdata:/data:rw\
-                     -v /home/fkalter/km/log:/log\
-                     -d -p 4001:4001\
-                     freekkalter/km:{} /usr/bin/supervisord".format(cidfile, buildNumber))
+    run("docker rm km_production")
+    run("docker run -name km_production -cidfile={}\
+                    -v /home/fkalter/km/postgresdata:/data:rw\
+                    -v /home/fkalter/km/log:/log\
+                    -d -p 4001:4001 \
+                    freekkalter/km:{} /usr/bin/supervisord".format(cidfile, buildNumber))
