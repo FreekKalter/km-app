dd6371e489626cc490faeaebe9d977467dc69162
Freek Kalter <freek@kalteronline.org>
2014-03-31 17:42:57 +0200

    try to epxort data into excel with markup

--- xls/xls.py
+++ xls/xls.py
@@ -1,7 +1,18 @@
 import xlsxwriter
 import json
 import httplib
+from os.path import join
+from xlrd import open_workbook
 
+from xlutils.copy import copy
+rb= open_workbook('uren_template.xlsx',   on_demand=True)
+
+wb = copy(rb)
+
+ws = wb.get_sheet(0)
+ws.write(2,7, 'hello')
+
+wb.save('output.xlsx')
 # get data and decodo json
 conn = httplib.HTTPConnection("localhost", 4001)
 conn.request("GET", "/overview/tijden/2014/2")
@@ -13,28 +24,29 @@ d= resp.read()
 data = json.loads(d)
 #print data
 
-# make work book and define some styles for formatting
-workbook = xlsxwriter.Workbook('hello.xlsx')
+## make work book and define some styles for formatting
+workbook = xlsxwriter.Workbook('out.xlsx')
 worksheet = workbook.add_worksheet()
-bold = workbook.add_format({'bold':True})
-grayBg = workbook.add_format({'bg_color': 'gray'})
+#print workbook.worksheets()
 
-lightGray = '#707070'
-header = bold
-header.set_bg_color(lightGray)
-header.set_bottom(1)
+#lightGray = '#707070'
+#bold = workbook.add_format({'bold':True})
+#grayBg = workbook.add_format({'bg_color': 'gray'})
 
-firstColumn = bold
-firstColumn.set_bg_color(lightGray)
-firstColumn.set_right(1)
+#boldGray = bold
+#boldGray.set_bg_color(lightGray)
 
+#header = boldGray
+#header.set_bottom(1)
+#for x,item in enumerate(data[0]):
+    #worksheet.write(0,x+1, item, boldGray)
 
+#firstColumn = boldGray
+#firstColumn.set_right(1)
+#for y in range(len(data)):
+    #worksheet.write(y+1,0, y+1,boldGray)
 
 for y,row in enumerate(data):
     for x,item in enumerate(row):
-        if (x ==0):
-            worksheet.write(y,x, y, firstColumn)
-        if (y == 0):
-            worksheet.write(y,x, item, header)
         worksheet.write(y+1,x+1, row[item])
 workbook.close()
